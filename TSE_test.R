0a747e15eb3a9a747205a25174462a54b7886b41
Evgeny <51246169+evgelver@users.noreply.github.com>
2020-10-01 13:26:58 +0300

    Add files via upload

--- TSE_test.R
+++ TSE_test.R
@@ -27,6 +27,7 @@ event_seq <- seqecreate(
   time=actcal.tse$time, 
   event=actcal.tse$event
 )
+event_seq[[4]]
 
 # поиск частых подпоследовательностей
 fsubseq <- seqefsub(event_seq, min.support=20, pmin.support=1e-5)
@@ -34,12 +35,12 @@ print(fsubseq)
 
 # датафрейм: в строках ИД последовательностей, 
 # в столбцах - признаки наличия частых подпоследовательностей
-data = data.frame(seq_id = unique(actcal.tse$id),seqeapplysub(fsubseq, method="presence")
+data <- data.frame(seq_id = unique(actcal.tse$id),seqeapplysub(fsubseq, method="presence")
 )
 
 # Расстояние между подпоследовательностями (расстояние Левенштейна)
-seq_data = seqdef(data)
-OM = seqdist(seq_data, method = 'OM', sm=seqsubm(seq_data, method = "CONSTANT"))
+seq_data <- seqdef(data)
+OM <- seqdist(seq_data, method = 'OM', sm=seqsubm(seq_data, method = "CONSTANT"))
 
 # Время окончания последовательности состояний
 end_time <- function(tse){
@@ -56,25 +57,25 @@ stm <- seqe2stm(events, dropList=list(PartTime=events[-1],
       Stop=events[-5], LowPartTime=events[-6], Increase=events[-7], Decrease=events[-8]))
 
 # Из формата TSE в формат STS
-actcal.sts = TSE_to_STS(actcal.tse, id=1, timestamp=2, event=3,
-                        stm=stm, tmin=1, tmax=end_time(actcal.tse), firstState="None"):
-actcal.seq = seqdef(actcal.sts, informat = 'STS')
-actcal.seq
+actcal.sts <- TSE_to_STS(actcal.tse, id=1, timestamp=2, event=3,
+                        stm=stm, tmin=1, tmax=end_time(actcal.tse), firstState="None")
+actcal.seq <- seqdef(actcal.sts, informat = 'STS')
 
 # 2 метрики:
 # 1.the Longest Common Prefix LCP
 # 2.the Optimal Mathing distances (OM)
-LCP_dist = seqdist(actcal.seq, method = "LCP", norm = 'auto')
-OM_dist = seqdist(actcal.seq, method = "OM", norm = 'auto', 
+LCP_dist <- seqdist(actcal.seq, method = "LCP", norm = 'auto')
+OM_dist <- seqdist(actcal.seq, method = "OM", norm = 'auto', 
                   sm=seqsubm(actcal.seq, method = "CONSTANT"))
 
-# Кластеризация
-library(cluster)
 
-clusterward <- agnes(LCP_dist, diss = TRUE, method = "ward")
-plot(clusterward, which.plots = 2)
+# суммирование матриц (матрица подпоследовательностей с коэффицентом)
+# w - коэффицент
+# OM - матрица расстояний (STS)
+# OM_subseq - матрица расстояний (Subseq)
+result_matrix <- function(w,OM,OM_subseq) {
+  return(OM+w*OM_subseq)
+} 
 
-cluster3 <- cutree(clusterward, k = 3)
-cluster3 <- factor(cluster3, labels = c("Type 1", "Type 2", "Type 3"))
-table(cluster3)
-seqfplot(actcal.seq, group = cluster3, pbarw = T)
+# запись файла
+write.table(result_matrix(3,OM_dist,OM), file="Result_df_toy_coef.csv")
